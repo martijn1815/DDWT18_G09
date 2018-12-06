8936084155f02bd937c6d9dfd8a3c3a6ba7f7aca
Martijn Schendstok <sch.martijn@outlook.com>
2018-12-06 22:21:09 +0100

    Added base documentes

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Controller
+ * User: martijn1815
+ * Date: 06-11-18
+ * Time: 22:20
+ */
+
---
+++ model.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Model
+ * User: martijn1815
+ * Date: 06-11-18
+ * Time: 22:20
+ */
+
+/* Enable error reporting */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
