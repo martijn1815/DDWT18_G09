07b4c39929d8425bc97785f48414c82c27bbd8cf
JuliusSyt <julius.s92@gmail.com>
2018-12-14 21:03:45 +0100

    Added in model: function to connect with DB, function to get navigation, function get breadcrumbs, functions new route, function na. Added to index: landing page set-up with navigation (no design), navigation_template for all pages, please check the routes and / or names of routes

--- index.php
+++ index.php
@@ -6,4 +6,52 @@
  * Time: 22:20
  */
 
-echo "test";
+$navigation_template = Array(
+    1 => Array(
+        'name' => 'Home',
+        'url' => '/finalproject/'
+    ),
+    2 => Array(
+        'name' => 'Register',
+        'url' => '/finalproject/register/'
+    ),
+    3 => Array(
+        'name' => 'Login',
+        'url' => '/finalproject/login/'
+    ),
+    4 => Array(
+        'name' => 'User Profile',
+        'url' => '/finalproject/userprofile/'
+    ),
+    5 => Array(
+        'name' => 'Rooms Overview',
+        'url' => '/finalproject/roomsoverview/'
+    ),
+    6 => Array(
+        'name' => 'Add Rooms',
+        'url' => '/finalproject/addrooms/'
+    ),
+    7 => Array(
+        'name' => 'My Rooms',
+        'url' => '/finalproject/myrooms/'
+    ),
+);
+
+/* Landing page */
+if (new_route('/DDWT18/week2/', 'get')) {
+
+    /* Page info */
+    $page_title = 'Home';
+    $breadcrumbs = get_breadcrumbs([
+        'finalproject' => na('/finalproject/', True),
+    ]);
+
+    $navigation = get_navigation($navigation_template, 1);
+
+    /* Page content */
+    $page_subtitle = 'The online platform to view and offer student rooms';
+    $page_content = 'content';
+
+    /* Choose Template */
+
+}
