cbb7b3081ec627800281458e5ee3e5bb2dd48723
Martijn Schendstok <sch.martijn@outlook.com>
2019-01-07 14:17:50 +0100

    change in the db

--- db/ddwt18_g09.sql
+++ db/ddwt18_g09.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 06, 2019 at 07:04 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Gegenereerd op: 07 jan 2019 om 13:16
+-- Serverversie: 5.7.23
+-- PHP-versie: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,61 +19,14 @@ SET time_zone = "+00:00";
 --
 -- Database: `ddwt18_g09`
 --
-CREATE DATABASE IF NOT EXISTS `ddwt18_g09` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `ddwt18_g09`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `languages`
+-- Tabelstructuur voor tabel `users`
 --
 
-CREATE TABLE `languages` (
-  `user_id` int(11) NOT NULL,
-  `language` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `opt_in`
---
-
-CREATE TABLE `opt_in` (
-  `id` int(11) NOT NULL,
-  `tenant_id` int(11) NOT NULL,
-  `message` varchar(256) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rooms`
---
-
-CREATE TABLE `rooms` (
-  `id` int(11) NOT NULL,
-  `owner_id` int(11) NOT NULL,
-  `size_m2` int(11) NOT NULL,
-  `zip` varchar(6) NOT NULL,
-  `street` varchar(256) NOT NULL,
-  `city` varchar(256) NOT NULL,
-  `description` text NOT NULL,
-  `type` varchar(256) NOT NULL,
-  `available_from` date NOT NULL,
-  `available_till` date NOT NULL,
-  `furnished` varchar(256) NOT NULL,
-  `price` int(11) NOT NULL,
-  `services_including` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(256) NOT NULL,
@@ -89,83 +42,28 @@ CREATE TABLE `users` (
   `date_of_birth` date NOT NULL,
   `role` varchar(256) NOT NULL,
   `gender` varchar(256) NOT NULL,
-  `study` varchar(256) NOT NULL,
   `profession` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexes for table `languages`
---
-ALTER TABLE `languages`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `opt_in`
---
-ALTER TABLE `opt_in`
-  ADD PRIMARY KEY (`id`,`tenant_id`),
-  ADD KEY `tenant_id` (`tenant_id`);
-
---
--- Indexes for table `rooms`
---
-ALTER TABLE `rooms`
-  ADD PRIMARY KEY (`id`,`owner_id`),
-  ADD KEY `owner_id` (`owner_id`);
-
---
--- Indexes for table `users`
+-- Indexen voor tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`,`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
 --
--- AUTO_INCREMENT for table `opt_in`
---
-ALTER TABLE `opt_in`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `rooms`
---
-ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `languages`
---
-ALTER TABLE `languages`
-  ADD CONSTRAINT `languages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `opt_in`
---
-ALTER TABLE `opt_in`
-  ADD CONSTRAINT `opt_in_ibfk_1` FOREIGN KEY (`tenant_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `rooms`
---
-ALTER TABLE `rooms`
-  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
