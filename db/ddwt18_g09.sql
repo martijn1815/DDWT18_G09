28355592dfbffcef5266a09f9a19a00a750abcc9
Martijn Schendstok <sch.martijn@outlook.com>
2019-01-08 13:59:22 +0100

    updated db

--- db/ddwt18_g09.sql
+++ db/ddwt18_g09.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Gegenereerd op: 07 jan 2019 om 16:04
+-- Gegenereerd op: 08 jan 2019 om 12:58
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -33,6 +33,15 @@ CREATE TABLE `languages` (
   `language` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `languages`
+--
+
+INSERT INTO `languages` (`user_id`, `language`) VALUES
+(5, 'dutch'),
+(5, 'english'),
+(5, 'french');
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +64,7 @@ CREATE TABLE `opt_in` (
 CREATE TABLE `rooms` (
   `id` int(11) NOT NULL,
   `owner_id` int(11) NOT NULL,
+  `room_title` varchar(256) NOT NULL,
   `size_m2` int(11) NOT NULL,
   `zip` varchar(6) NOT NULL,
   `street` varchar(256) NOT NULL,
@@ -67,6 +77,19 @@ CREATE TABLE `rooms` (
   `price` int(11) NOT NULL,
   `services_including` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `owner_id`, `room_title`, `size_m2`, `zip`, `street`, `city`, `description`, `type`, `available_from`, `available_till`, `furnished`, `price`, `services_including`) VALUES
+(2, 5, 'test', 15, '0000aa', 'test 1', 'test', 'test', 'studenthouse', '2019-01-01', '2019-08-01', 'no', 350, 'no'),
+(3, 5, 'test2', 15, '0000aa', 'test 2', 'test', 'test2', 'studenthouse', '2019-01-01', '2019-08-01', 'yes', 350, 'yes'),
+(4, 5, 'test3', 18, '0000aa', 'test3', 'test', 'test3', 'ownershouse', '2018-01-01', '2019-01-01', 'no', 250, 'yes'),
+(5, 5, 'test4', 13, '0000aa', 'test 4', 'test', 'test', 'ownershouse', '2018-01-01', '2019-01-01', 'yes', 150, 'no'),
+(6, 5, 'test5', 3, '0000aa', 'test 5', 'test', 'test', 'studenthouse', '2018-01-01', '2019-01-01', 'no', 50, 'yes'),
+(7, 5, 'test6', 5, '0000aa', 'test 6', 'test', 'test', 'studenthouse', '2018-01-01', '2019-01-01', 'yes', 50, 'no'),
+(8, 5, 'test 01', 5, '0000aa', 'tes 01', 'test', 'test', 'studenthouse', '2018-01-01', '2019-01-01', 'no', 50, 'yes');
 
 -- --------------------------------------------------------
 
@@ -83,7 +106,7 @@ CREATE TABLE `users` (
   `zip` varchar(6) NOT NULL,
   `street` varchar(256) NOT NULL,
   `city` varchar(256) NOT NULL,
-  `phone_number` int(10) NOT NULL,
+  `phone_number` int(15) NOT NULL,
   `email` varchar(256) NOT NULL,
   `biography` text NOT NULL,
   `date_of_birth` date NOT NULL,
@@ -91,6 +114,13 @@ CREATE TABLE `users` (
   `gender` varchar(256) NOT NULL,
   `profession` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `zip`, `street`, `city`, `phone_number`, `email`, `biography`, `date_of_birth`, `role`, `gender`, `profession`) VALUES
+(5, 'test', '$2y$10$mfMsKnPZFU46.Obv8IWyietb7LDQvvVyoI3QuoPSpAjpDzrGjo6d.', 'test', 'test', '0000aa', 'test 1', 'test', 645925691, 'test@test.test', 'test', '2000-11-11', 'tenant', 'male', 'test');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -130,13 +160,13 @@ ALTER TABLE `opt_in`
 -- AUTO_INCREMENT voor een tabel `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
